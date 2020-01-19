310d43f8416a66839a41de3e73801c6da2b691a4
Arjan van Eerden <Arjanvaneerden@gmail.com>
2020-01-20 00:02:23 +0100

    reading csv data

--- amazon.py
+++ amazon.py
@@ -1,12 +1,49 @@
 import pandas as pd
 
+class Reply:
+    def __init__(self, Reactie, ReactieLikes, ReactieDatum, ReactieDoor):
+        self.Reactie = Reactie
+        self.ReactieLikes = ReactieLikes
+        self.ReactieDatum = ReactieDatum
+        self.ReactieDoor = ReactieDoor
+
 
 class Post:
-    def __init__(self):
-        self.content = ""
+    def __init__(self, Microblog, MicroblogLikes, Created, Door, isPoll):
+        self.Microblog = Microblog
+        self.MicroblogLikes = MicroblogLikes
+        self.Created = Created
+        self.Door = Door
+        self.isPoll = isPoll
+        self.Reacties = []
 
+    #def __str__(self):
+    #    return("Post:", self.Microblog,
+    #    "\n of User: ", self.Door.getName())
 
+    def addReaction(self, Reactie, ReactieLikes, ReactieDatum, ReactieDoor):
+        self.Reacties.append(Reply(Reactie, ReactieLikes, ReactieDatum, ReactieDoor))
+           
 class User:
-    def __init__(self):
-        self.username = ''
-        self.posts = []
+    def __init__(self, FullName, Function='', LastLogin='', IsExternal='', ManagerLevel='', IsAccountManager='',
+    HasAvatar='', HasPhonenumber='', Summary='', DateOfBirth='', EmployeeSince='', Organization='', posts=[]):
+        self.FullName = FullName
+        self.Function = Function
+        self.LastLogin = LastLogin
+        self.IsExternal = IsExternal
+        self.ManagerLevel = ManagerLevel
+        self.IsAccountManager = IsAccountManager
+        self.HasAvatar = HasAvatar
+        self.HasPhonenumber = HasPhonenumber
+        self.Summary = Summary
+        self.DateOfBirth = DateOfBirth
+        self.EmployeeSince = EmployeeSince
+        self.Organization = Organization
+        self.posts = posts
+
+    #def __str__(self):
+    #    return("Full Name:", self.FullName,
+    #    "\n Posts: ", self.posts)
+    
+    def getName(self):
+        return(self.FullName)
---
+++ readdata.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from amazon import *
+import sys
+
+VERBOSE = True
+
+
+def readUsers(userCSV):
+    # read csv with the users
+    userDF = pd.read_csv(userCSV, encoding='utf-8')
+    # create a dictionary of user-objects, e.g. John Johnsen <amazon.Use object>
+    userDict = {u.FullName : User(
+        u.FullName, u.Function, u.LastLogin, u.IsExternal, u.ManagerLevel, u.IsAccountManager,
+        u.HasAvatar, u.HasPhonenumber, u.Summary, u.DateOfBirth, u.EmployeeSince, u.Organization
+        ) for u in userDF.itertuples()}
+    # Sandra Schuur was missing:
+    userDict['Sandra Schuur'] = User('Sandra Schuur')
+    # check if all users are there:
+    if VERBOSE:
+        print("\nUsers in dictionary:")
+        for key, value in userDict.items() :
+            print (key, value)
+    return userDict
+
+
+def readBlogs(microblogs, polls, userDict):
+    # read the csv with the microblogs
+    blogsDF = pd.read_csv(microblogs, encoding='utf-8')
+    # we use polls.csv to check whether a microblog is a 'post' or a 'poll'
+    pollsDF = pd.read_csv(polls, encoding='utf-8')
+    #list of polls
+    allUniquePolls = set(pollsDF['Microblog'].tolist())
+    
+    # adding blogs to dictionary-objects
+    blogsDict = {}
+    for _,row in blogsDF.iterrows():
+        Microblog = row['Microblog']
+        if VERBOSE:
+            print('\n', Microblog)
+
+        # if the microblog DOES NOT EXIST in blogsdict, create a new object in the dictionary
+        if Microblog not in blogsDict:
+
+            # if the microblog exists in polls.csv it is a poll
+            isPoll = Microblog in allUniquePolls
+            blogsDict[Microblog] = Post(Microblog, row['MicroblogLikes'], row['Created'], userDict[row['Door']], isPoll)
+            
+        # add reaction
+        if not (pd.isnull(row['Reactie']) or pd.isnull(row['ReactieLikes'])): # only add if 'Reactie' and likes are not NULL
+            blogsDict[Microblog].addReaction(row['Reactie'], row['ReactieLikes'], row['ReactieDatum'], userDict[row['ReactieDoor']])
+        if VERBOSE:
+            print(row['Reactie'])
+            
+def main():
+    userDict = readUsers('persons.csv')
+    readBlogs('microblogs.csv', 'polls.csv', userDict)
+
+if __name__ == "__main__":
+    main()
